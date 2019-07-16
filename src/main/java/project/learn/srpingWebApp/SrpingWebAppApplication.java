3fb950fc0ecc6e51b345a6bd4b70e105ddb7e6cb
mbogdan2704 <costin_bogdan.maria@stud.acs.upb.ro>
2019-07-16 12:19:35 +0300

    Add files via upload

---
+++ src/main/java/project/learn/srpingWebApp/SrpingWebAppApplication.java
@@ -0,0 +1,13 @@
+package project.learn.srpingWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/learn/srpingWebApp/SrpingWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package project.learn.srpingWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
